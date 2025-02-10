b20d74bcc01c4035e65a51bca864c84aff1c5167
xxyzz <gitpull@protonmail.com>
2025-02-10 20:04:50 +0800

    Create Katakana files

--- kana.py
+++ kana.py
@@ -1,4 +1,3 @@
-import subprocess
 from pathlib import Path
 from dataclasses import dataclass
 
@@ -9,7 +8,7 @@ import genanki
 @dataclass
 class Td:
     chinese: str = ""
-    hiragana: str = ""
+    kana: str = ""
     audio: str = ""
     romanization: str = ""
     stroke_order: str = ""
@@ -21,7 +20,7 @@ class Row:
     td_list: list[Td]
 
 
-def create_anki_deck(rows: list[Row]) -> None:
+def create_hiragana_deck(rows: list[Row]) -> None:
     model = genanki.Model(
         2145308593,
         "Hiragana",
@@ -82,30 +81,26 @@ span[lang="zh"] {
                 model=model,
                 fields=[
                     td.chinese,
-                    td.hiragana,
-                    f"[sound:sounds/{td.audio}]" if td.audio != "" else "",
+                    td.kana,
+                    f"[sound:{td.audio}]" if td.audio != "" else "",
                     td.romanization,
-                    f'<img src="images/{td.stroke_order}">' if td.stroke_order != "" else "",
+                    f'<img src="{td.stroke_order}">' if td.stroke_order != "" else "",
                 ],
             )
             deck.add_note(note)
+            if td.audio != "":
+                package.media_files.append(f"sounds/{td.audio}")
+            if td.stroke_order != "":
+                package.media_files.append(f"images/{td.stroke_order}")
 
     package.media_files.extend(
         ["fonts/_HengShanMaoBiCaoShu-2.ttf", "fonts/_KleeOne-Regular.ttf"]
     )
-    package.write_to_file("hiragana.apkg")
+    package.write_to_file("_site/hiragana.apkg")
 
 
-if __name__ == "__main__":
-    env = Environment(
-        loader=PackageLoader("kana"),
-        autoescape=select_autoescape(),
-        trim_blocks=True,
-        lstrip_blocks=True,
-    )
-    template = env.get_template("hiragana.html")
-
-    Path("_site").mkdir(exist_ok=True)
+def create_hiragana_files(env):
+    hiragana_template = env.get_template("hiragana.html")
     with open("_site/hiragana.html", "w", encoding="utf-8") as f:
         rows = [
             Row(
@@ -209,5 +204,212 @@ if __name__ == "__main__":
                 ],
             ),
         ]
-        f.write(template.render(rows=rows))
-        create_anki_deck(rows)
+        f.write(hiragana_template.render(rows=rows))
+        create_hiragana_deck(rows)
+
+
+def create_katakana_files(env):
+    katakana_template = env.get_template("katakana.html")
+    with open("_site/katakana.html", "w", encoding="utf-8") as f:
+        rows = [
+            Row(
+                "",
+                [
+                    Td("阿", "ア", "Ja-A.oga", "a", "ア-bw.svg"),
+                    Td("伊", "イ", "", "i", "イ-bw.svg"),
+                    Td("宇", "ウ", "Ja-U.oga", "u", "ウ-bw.svg"),
+                    Td("江", "エ", "Ja-E.oga", "e", "エ-bw.svg"),
+                    Td("於", "オ", "Ja-O.oga", "o", "オ-bw.svg"),
+                ],
+            ),
+            Row(
+                "k",
+                [
+                    Td("加", "カ", "Ja-ka.ogg", "ka", "カ-bw.svg"),
+                    Td("幾", "キ", "", "ki", "キ-bw.svg"),
+                    Td("久", "ク", "", "ku", "ク-bw.svg"),
+                    Td("介", "ケ", "", "ke", "ケ-bw.svg"),
+                    Td("己", "コ", "", "ko", "コ-bw.svg"),
+                ],
+            ),
+            Row(
+                "s",
+                [
+                    Td("散", "サ", "", "sa", "サ-bw.png"),
+                    Td("之", "シ", "Ja-Shi.oga", "shi", "シ-bw.png"),
+                    Td("須", "ス", "", "su", "ス-bw.png"),
+                    Td("世", "セ", "", "se", "セ-bw.png"),
+                    Td("曽", "ソ", "", "so", "ソ-bw.png"),
+                ],
+            ),
+            Row(
+                "t",
+                [
+                    Td("多", "タ", "", "ta", "タ-bw.png"),
+                    Td("千", "チ", "Ja-Chi_2.oga", "chi", "チ-bw.png"),
+                    Td("川", "ツ", "Ja-Tsu.oga", "tsu", "ツ-bw.png"),
+                    Td("天", "テ", "", "te", "テ-bw.png"),
+                    Td("止", "ト", "", "to", "ト-bw.png"),
+                ],
+            ),
+            Row(
+                "n",
+                [
+                    Td("奈", "ナ", "", "na", "ナ-bw.png"),
+                    Td("仁", "ニ", "Ja-2-ni.ogg", "ni", "ニ-bw.png"),
+                    Td("奴", "ヌ", "", "nu", "ヌ-bw.png"),
+                    Td("祢", "ネ", "", "ne", "ネ-bw.png"),
+                    Td("乃", "ノ", "", "no", "ノ-bw.png"),
+                ],
+            ),
+            Row(
+                "h",
+                [
+                    Td("八", "ハ", "", "ha", "ハ-bw.png"),
+                    Td("比", "ヒ", "Ja-Hi.oga", "hi", "ヒ-bw.png"),
+                    Td("不", "フ", "Ja-Fu.oga", "fu", "フ-bw.png"),
+                    Td("部", "ヘ", "", "he", "ヘ-bw.png"),
+                    Td("保", "ホ", "", "ho", "ホ-bw.png"),
+                ],
+            ),
+            Row(
+                "m",
+                [
+                    Td("末", "マ", "", "ma", "マ-bw.png"),
+                    Td("三", "ミ", "", "mi", "ミ-bw.png"),
+                    Td("牟", "ム", "", "mu", "ム-bw.png"),
+                    Td("女", "メ", "", "me", "メ-bw.png"),
+                    Td("毛", "モ", "", "mo", "モ-bw.png"),
+                ],
+            ),
+            Row(
+                "y",
+                [
+                    Td("也", "ヤ", "", "ya", "ヤ-bw.png"),
+                    Td(),
+                    Td("由", "ユ", "", "yu", "ユ-bw.png"),
+                    Td(),
+                    Td("與", "ヨ", "", "yo", "ヨ-bw.png"),
+                ],
+            ),
+            Row(
+                "r",
+                [
+                    Td("良", "ラ", "Ja-Ra.oga", "ra", "ラ-bw.png"),
+                    Td("利", "リ", "Ja-Ri.oga", "ri", "リ-bw.png"),
+                    Td("流", "ル", "Ja-Ru.oga", "ru", "ル-bw.png"),
+                    Td("礼", "レ", "Ja-Re.oga", "re", "レ-bw.png"),
+                    Td("呂", "ロ", "Ja-Ro.oga", "ro", "ロ-bw.png"),
+                ],
+            ),
+            Row(
+                "w",
+                [
+                    Td("和", "ワ", "", "wa", "ワ-bw.png"),
+                    Td(""),
+                    Td("尓", "ン", "", "n", "ン-bw.png"),
+                    Td(),
+                    Td("乎", "ヲ", "Ja-O.oga", "o", "ヲ-bw.png"),
+                ],
+            ),
+        ]
+        f.write(katakana_template.render(rows=rows))
+        create_katakana_deck(rows)
+
+
+def create_katakana_deck(rows: list[Row]) -> None:
+    model = genanki.Model(
+        1630224618,
+        "Katakana",
+        fields=[
+            {"name": "Chinese"},
+            {"name": "Chinese_svg"},
+            {"name": "Katakana"},
+            {"name": "audio"},
+            {"name": "romanization"},
+            {"name": "stroke_order"},
+        ],
+        templates=[
+            {
+                "name": "Katakana",
+                "qfmt": '<span lang="ja">{{Katakana}}</span>',
+                "afmt": '{{FrontSide}}<p>{{Chinese_svg}} <span lang="zh">{{Chinese}}</span></p><p>{{romanization}}</p><p>{{audio}}</p><p>{{stroke_order}}</p>',
+            }
+        ],
+        css="""
+@font-face {
+  font-family: "hengshancaoshu";
+  src: url("_HengShanMaoBiCaoShu-2.ttf");
+}
+
+@font-face {
+  font-family: "Klee One";
+  src: url("_KleeOne-Regular.ttf");
+}
+
+.card {
+  font-size: xxx-large;
+  text-align: center;
+}
+
+p {
+  margin: auto;
+}
+
+span[lang="ja"] {
+  font-family: "Klee One", serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+span[lang="zh"] {
+  font-family: "hengshancaoshu", serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.zh-svg {
+  width: 3rem;
+}
+""",
+    )
+    deck = genanki.Deck(1492529969, "Katakana")
+    package = genanki.Package(deck)
+    for row in rows:
+        for td in row.td_list:
+            if td.chinese == "":
+                continue
+            note = genanki.Note(
+                model=model,
+                fields=[
+                    td.chinese,
+                    f'<img class="zh-svg" src="{td.chinese}_KleeOne-Regular.svg">',
+                    td.kana,
+                    f"[sound:{td.audio}]" if td.audio != "" else "",
+                    td.romanization,
+                    f'<img src="{td.stroke_order}">' if td.stroke_order != "" else "",
+                ],
+            )
+            deck.add_note(note)
+            package.media_files.append(f"images/{td.chinese}_KleeOne-Regular.svg")
+            if td.audio != "":
+                package.media_files.append(f"sounds/{td.audio}")
+            if td.stroke_order != "":
+                package.media_files.append(f"images/{td.stroke_order}")
+
+    package.media_files.extend(
+        ["fonts/_HengShanMaoBiCaoShu-2.ttf", "fonts/_KleeOne-Regular.ttf"]
+    )
+    package.write_to_file("_site/katakana.apkg")
+
+
+if __name__ == "__main__":
+    env = Environment(
+        loader=PackageLoader("kana"),
+        autoescape=select_autoescape(),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    Path("_site").mkdir(exist_ok=True)
+    create_hiragana_files(env)
+    create_katakana_files(env)
